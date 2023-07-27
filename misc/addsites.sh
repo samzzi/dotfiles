8d7ac93c7cf99b4a6e31ae43c4f541d4a62d5e46
Sam Serrien <sam@genx.be>
2023-07-27 13:11:19 +0200

    remove addsites

--- misc/addsites.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Cloning repositories..."
-
-SITES=$HOME/workspaces
-
-# Kirby
-git clone git@github.com:genxbe/kx-sitekit.git $SITES/kirby/kx-sitekit
-cd $SITES/kirby/kx-sitekit && herd link sitekit
